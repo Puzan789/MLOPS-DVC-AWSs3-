cb1afad54e8b1d86d71d5952cad5f696a1d3e332
puzan789 <puzan936@gmail.com>
2025-07-07 07:11:48 +0545

    Commiting

--- github/first.py
+++ github/first.py
@@ -1,1 +1,15 @@
 print("Helloworld")
+b=10
+c=20
+d=b+c
+print(d)
+a = 5
+if a > 0:
+    print("a is positive")
+else:
+    print("a is not positive")
+for i in range(5):
+    print(i)
+while a > 0:
+    print("a is still positive")
+    a -= 1
