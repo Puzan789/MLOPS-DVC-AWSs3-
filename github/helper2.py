ccd885305a5ee1be6bfb5d23b0b46d674cff65ef
puzan789 <puzan936@gmail.com>
2025-07-07 07:34:48 +0545

    no8

---
+++ github/helper2.py
@@ -0,0 +1,8 @@
+print("hello world from helper2")
+def helper2_function():
+    print("This is a helper2 function")
+    return "Helper2 function executed successfully"
+str2 = "Helper2 string is also amazing"
+def print_helper2_string(s):
+    print(s)
+om2 = "this is helper2 string"
